9a3070bc27fd91dc4db00ecdb8911788611e679b
Klhee Jacobs <bangklhee@gmail.com>
2022-12-21 23:39:11 -0600

    Jupyter notebook Please enter the commit message for your changes. Lines starting

---
+++ assignment_1.py
@@ -0,0 +1,13 @@
+l_1 = [1,11,14,5,8,9,23]
+
+def new_list():
+    for number in l_1:
+        if number>10:
+            return False
+        else:
+            number<10
+            return True
+    while True:
+        print(number)
+    
+new_list()
--- assignment_2.py
+++ assignment_2.py
@@ -1,28 +1,41 @@
-# Exercise 1
-# Given a list as a parameter,write a function that returns a list of numbers that are less than ten
-# For example: Say your input parameter to the function is [1,11,14,5,8,9]...Your output should [1,5,8,9]
-# Use the following list - [1,11,14,5,8,9]
+# # Exercise 1
+# # Given a list as a parameter,write a function that returns a list of numbers that are less than ten
+# # For example: Say your input parameter to the function is [1,11,14,5,8,9]...Your output should [1,5,8,9]
+# # Use the following list - [1,11,14,5,8,9]
 
-l_1 = [1,11,14,5,8,9]
-l_2 =[]
+# l_1 = [1,11,14,5,8,9]
+# l_2 =[]
 
-def new_list():
-    for i in range(len(l_1)):
-        if l_1[i] <= 10:
-            l_2.append(l_1[i])
-    print(l_2)
+# def new_list():
+#     for i in range(len(l_1)):
+#         if l_1[i] <= 10:
+#             l_2.append(l_1[i])
+#     print(l_2)
 
-new_list()
+# new_list()
 
 
-# Exercise 2
-# Write a function that takes in two lists and returns the two lists merged together and sorted
-# Hint: You can use the .sort() method
+# # Exercise 2
+# # Write a function that takes in two lists and returns the two lists merged together and sorted
+# # Hint: You can use the .sort() method
 
-list_1 = [1,2,3,4,5,6]
-list_2 = [3,4,5,6,7,8,10]
+# list_1 = [1,2,3,4,5,6]
+# list_2 = [3,4,5,6,7,8,10]
 
-def list_3():
-    list_3 = sorted(list_1 + list_2)
-    print(list_3)
-list_3()
+# def list_3():
+#     list_3 = sorted(list_1 + list_2)
+#     print(list_3)
+# list_3()
+
+first_name = ['John', 'Evan', 'Jordan', 'Max']
+last_name = ['Smith', 'Smith', 'Williams', 'Bell']
+
+new_name = []
+
+def Output():
+    for i in range(4):
+        full_name = first_name[i] + " " + last_name[i]
+        new_name.append(full_name)
+    print(new_name)
+    
+Output()
